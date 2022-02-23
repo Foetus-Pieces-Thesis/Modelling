ee1950cee695709c8ddbbf9959dadda080c8ccb3
KONGNROY <roynsah@gmail.com>
2022-02-23 16:57:08 +0000

    v2.1 clustering behavior and random walk (fixed) + can now change parameters to simulate both behaviours
    > Clustering algorithm calculates the center of mass of a local neighbourhood (a shell) around a cell.
    
    The cell is attracted to this centre of mass by a force ~ n^2/((s^2+n)D^2), where:
    n = number of neighbouring cells
    s = standard deviation from center of mass (a measure of density or spread)
    D = distance to center
    
    This is repeated for a series of shells with increasing radius and the total force is calculated.
    ...
    
    I hypothesised that this would produce stable clusters.
    
    - Clustering is present but can be unstable -> need to add rule to stabalise the clusters

--- Random walk/Assets/Scripts/generateCells.cs
+++ Random walk/Assets/Scripts/generateCells.cs
@@ -4,20 +4,13 @@ using UnityEngine;
 
 public class generateCells : MonoBehaviour
 {
-<<<<<<< HEAD
     [Range(1, 10000)]
     public int NumberOfCells = 100;
     [Range(1, 50)]
     public int maxInitDist = 10;
     public GameObject cell;
-    
-    
-=======
-    public int NumberOfCells = 100;
-    public int maxInitDist = 10;
-    public GameObject cell;
-    
->>>>>>> 9a661de443f1963005796c3788be5320a0ba10bf
+
+
     void Start()
     {
         InitializeRandom();
@@ -25,17 +18,13 @@ public class generateCells : MonoBehaviour
 
     void InitializeRandom()
     {
-        for (int i= 0; i < NumberOfCells; i++)
+        for (int i = 0; i < NumberOfCells; i++)
         {
-            int randNum = Random.Range(-maxInitDist, maxInitDist+1);
-<<<<<<< HEAD
+            int randNum = Random.Range(-maxInitDist, maxInitDist + 1);
             Vector3 InitPos = Random.insideUnitSphere * randNum;
             GameObject e = Instantiate(cell, InitPos, Random.rotation) as GameObject;
-=======
-            GameObject e = Instantiate(cell, Random.insideUnitSphere * randNum, Random.rotation) as GameObject;
-            //e.transform.position = 
->>>>>>> 9a661de443f1963005796c3788be5320a0ba10bf
         }
     }
 
 }
+
