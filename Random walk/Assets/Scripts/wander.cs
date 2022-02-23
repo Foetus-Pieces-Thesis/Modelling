ece1fda0ccc28cc7a9bb1962325799f44e584195
KONGNROY <roynsah@gmail.com>
2022-02-23 18:07:01 +0000

    v2.1.1 clustering behavior and random walk (fixed + some problems) + can now change parameters to simulate both behaviours
    > Clustering algorithm calculates the center of mass of a local neighbourhood (a shell) around a cell.
    
    The cell is attracted to this centre of mass by a force ~ n^2/((s^2+n)D^2), where:
    n = number of neighbouring cells
    s = standard deviation from center of mass (a measure of density or spread)
    D = distance to center
    
    This is repeated for a series of shells with increasing radius and the total force is calculated.
    ...
    
    I hypothesised that this would produce stable clusters.
    
    - Clustering is present but can be unstable -> need to add rule to stabalise the clusters
    - Problem with Update and FixedUpdate related to framerate -> need to revise code and its validity as a model

--- Random walk/Assets/Scripts/wander.cs
+++ Random walk/Assets/Scripts/wander.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class wander : MonoBehaviour{
 
-<<<<<<< HEAD
     [Range(0,100)]
     public float maxSpeed = 5f;
     [Range(0,100)]
@@ -27,33 +26,11 @@ public class wander : MonoBehaviour{
     private Vector3 nextPosition;
     private Vector3 velocity;
     private Vector3 desiredDirection;
-=======
-
-    [Range(0,100)]
-    public float maxSpeed = 20;
-    [Range(0,100)]
-    public float steerStrength = 0.2f;
-    [Range(0,10)]
-    public float wanderStrength = 0.1f;
-    [Range(1,10)]
-    public float viewRadius = 1.5f;
-    [Range(0, 1)]
-    public float cohesion = 1;
-    public GameObject daughter;
-    public LayerMask cellMask;
-    public LayerMask obstacleMask;
-    private Rigidbody rbody;
-
-    Vector3 position;
-    Vector3 velocity;
-    Vector3 desiredDirection;
->>>>>>> 9a661de443f1963005796c3788be5320a0ba10bf
 
     void Start()
     {
         //StartCoroutine("DoLocalEvolutionWithDelay", 10f);
         rbody = this.GetComponent<Rigidbody>();
-<<<<<<< HEAD
         pathLength = 0f;
         InitPos = rbody.position;
         nextPosition = InitPos;
@@ -123,7 +100,8 @@ public class wander : MonoBehaviour{
         /*Debug.Log("shell0: " + cellCount);*/
 
         Vector3 resultantForce = Vector3.zero;
-/*
+
+        /*
         if (cellCount != 0)
         {
             foreach (var cell in cellsInView)
@@ -156,11 +134,10 @@ public class wander : MonoBehaviour{
             nextPosition += (velocity * Time.deltaTime);// + RandomStep();
             Debug.Log("desiredPosition0: " + nextPosition);
         }
+        */
 
-*/
-
-        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
-        // +n- Fibonacci Shell
+        // n-shell
+        float goldenRatio = (1f + Mathf.Sqrt(5f)) / 2f;
         for (int n = 1; n < 5; n++)
         {
             float f = Mathf.Round(Mathf.Pow(goldenRatio, n + 1) / Mathf.Sqrt(5));//fibonacci number
@@ -201,14 +178,14 @@ public class wander : MonoBehaviour{
                 nextPosition += (velocity * Time.deltaTime);
             }
         }
-        //HandleDirectionalMovement();
 
+        //HandleMovement();
         Debug.Log("Update time: " + Time.deltaTime);
     }
 
     void FixedUpdate()
     {
-        HandleDirectionalMovement();   
+        HandleMovement();   
         Debug.Log("FixedUpdate time: " + Time.deltaTime);
     }
 
@@ -242,12 +219,11 @@ public class wander : MonoBehaviour{
     }
 
 
-    // Handles 3D pseudo random walk
-    // (Rule 0 - Baseline motion)
+    // Returns a Random step in 3D - determined by a random Force
     private Vector3 RandomStep()
     {
         
-        desiredDirection = (desiredDirection + Random.insideUnitSphere*wanderStrength).normalized;//pseudo-random nudge in direction (Order 1 Markov process)
+        desiredDirection = (desiredDirection + Random.insideUnitSphere*wanderStrength).normalized;//pseudo-random nudge of direction (Order 1 Markov process)
 
         Vector3 desiredVelocity = desiredDirection * (maxSpeed * RandomGaussian());//speed is normally distribution
         Vector3 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
@@ -255,201 +231,20 @@ public class wander : MonoBehaviour{
 
         velocity = Vector3.ClampMagnitude(velocity + (acceleration * Time.deltaTime), maxSpeed);//v = u + at
 
-        Vector3 randomStep = (velocity * Time.deltaTime);
-        //nextPosition += (velocity * Time.deltaTime);//s = vt
+        Vector3 randomStep = velocity * Time.deltaTime;//( Vector3.ClampMagnitude((acceleration * Time.deltaTime), maxSpeed) * Time.deltaTime );
 
-        //rbody.MovePosition(nextPosition);
-
-        /*Debug.Log("nextPosition-random: " + nextPosition);*/
-
-            // increment total path length
-
-            //pathLength += randomStep.magnitude;
-
-            return randomStep;
-=======
-        rbody.position = new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), Random.Range(-2, 2));
+        return randomStep;
     }
 
-
-    IEnumerator DoLocalEvolutionWithDelay(float delay)
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(delay);
-            DoLocalEvolution();
-        }
-    }
-
-    void DoLocalEvolution()
-    {
-        Collider[] cellsInNeighbourhood = Physics.OverlapSphere(this.transform.position, viewRadius, cellMask); // all cells inside local agent's neighbourhood
-        int noCellsAlive = 0;// cellsInNeighbourhood.Length;
-
-        foreach (var cell in cellsInNeighbourhood) { noCellsAlive += cell.GetComponent<MeshRenderer>().enabled ? 1 : 0; }
-
-        // Conway's Game of life Rules: 2333/5766
-        if (this.GetComponent<MeshRenderer>().enabled)
-        {
-            if (noCellsAlive < 2)// underpopulation
-            {
-                this.GetComponent<MeshRenderer>().enabled = false;
-                //Destroy(this);
-            }
-            else if (2 <= noCellsAlive || noCellsAlive <= 3)// lives on
-            {
-                //nothing
-            }
-            else if (noCellsAlive > 3) // overpopulation
-            {
-                this.GetComponent<MeshRenderer>().enabled = false;
-                //Destroy(this);
-            }
-
-        }
-        else if (noCellsAlive >= 2)//reproductive zone
-        {
-            //this.GetComponent<MeshRenderer>().enabled = true;
-            GameObject e = Instantiate(daughter) as GameObject;
-            e.transform.position = transform.position + Random.onUnitSphere;
-            //Destroy(this.gameObject);
-        }
-    }
-
-    void FixedUpdate()// move towards direction of closest neighbour
-    {
-        if (this.GetComponent<MeshRenderer>().enabled)
-        {
-            float disToClosestNeighbour = 0;
-            Vector3 dirToClosestNeighbour = new Vector3();
-
-            Collider[] neighboursInView = Physics.OverlapSphere(transform.position, viewRadius, cellMask);
-
-            foreach (var cell in neighboursInView)
-            {
-                if (Vector3.Distance(transform.position, cell.transform.position) < disToClosestNeighbour)
-                {
-                    disToClosestNeighbour = Vector3.Distance(transform.position, cell.transform.position);
-                    dirToClosestNeighbour = (cell.transform.position - transform.position).normalized;
-                }
-
-            }
-
-            // Dampen maxSpeed and steerStrength so motion towards neighbour doesn't halt general random motion
-            Vector3 desiredVelocity = dirToClosestNeighbour * maxSpeed * cohesion;
-            Vector3 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength * cohesion;
-            Vector3 acceleration = Vector3.ClampMagnitude(desiredSteeringForce, steerStrength) / 1; //normalised mass = 1
-
-            velocity = Vector3.ClampMagnitude(velocity + (acceleration * Time.deltaTime), maxSpeed);
-
-            rbody.position += (velocity * Time.deltaTime);
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(this.transform.position, viewRadius);
-    }
-
-    private void Update()
-    {
-        HandleMovement();
-    }
-
+    // Handles movement - clustering + random step 
     private void HandleMovement()
-    {
-        if (this.GetComponent<MeshRenderer>().enabled) 
-        {
-            desiredDirection = (desiredDirection + Random.insideUnitSphere * wanderStrength).normalized;// find distribution of random function
-
-            Vector3 desiredVelocity = desiredDirection * maxSpeed;
-            Vector3 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
-            Vector3 acceleration = Vector3.ClampMagnitude(desiredSteeringForce, steerStrength) / 1; //normalised mass = 1
-
-            velocity = Vector3.ClampMagnitude(velocity + (acceleration * Time.deltaTime), maxSpeed);
-            position += (velocity * Time.deltaTime);
-
-            float theta = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
-            float phi = Mathf.Atan2(velocity.z, velocity.x) * Mathf.Rad2Deg;
-
-            //transform.SetPositionAndRotation(position, Quaternion.Euler(0, phi, theta));
-            rbody.MovePosition(position);
-            rbody.MoveRotation(Quaternion.Euler(0, phi, theta));
-            
-        }
-        
->>>>>>> 9a661de443f1963005796c3788be5320a0ba10bf
-    }
-
-
-
-<<<<<<< HEAD
-
-    // Handles directional movement 
-    // (Rule 1 - Force applied by neighbouring cells = k/D^2, where D is the distance to neighbour)
-    private void HandleDirectionalMovement()
     {  
-        rbody.MovePosition(nextPosition + RandomStep());
+        rbody.MovePosition(nextPosition+RandomStep());
         // increment total path length
         pathLength += (velocity * Time.deltaTime).magnitude;
 
-/*
-        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
-        // +n- Fibonacci Shell
-        for (int n = 1; n < 7; n++)
-        {
-            float f = Mathf.Round(Mathf.Pow(goldenRatio, n + 1) / Mathf.Sqrt(5));//fibonacci number
-            cellsInView = Physics.OverlapSphere(rbody.position, viewRadius * f, cellMask);
-            cellCount = cellsInView.Length;
-            Vector3 toCenterOfCrowd = new Vector3();
-            float crowdSpread = 0f;
-
-
-            Debug.Log("shell" + n + ": " + cellCount);
-            if (cellCount >= 2)
-            {
-                foreach (var cell in cellsInView)
-                {
-                    toCenterOfCrowd += (cell.transform.position - rbody.position) / cellCount;// equivalent to a sample mean  
-                }
-                Debug.Log("toCenterofCrowd" + n + ": " + toCenterOfCrowd);
-
-                foreach (var cell in cellsInView)
-                {
-                    crowdSpread += ((cell.transform.position - rbody.position) - toCenterOfCrowd).sqrMagnitude / (cellCount - 1);// equivalent to a sample variance
-                }
-                Debug.Log("variance" + n + ": " + crowdSpread);
-
-                float distanceToCenter = toCenterOfCrowd.magnitude;
-                float virtualAuthority = (cellCount * cellCount) / (crowdSpread + cellCount);
-
-                Vector3 desiredVelocity = toCenterOfCrowd * (maxSpeed);
-                Vector3 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength * cohesion * virtualAuthority / (distanceToCenter * distanceToCenter);
-
-                resultantForce += desiredSteeringForce;
-                Debug.Log("desiredSteeringForce" + n + ": " + desiredSteeringForce);
-                Debug.Log("resultantForce" + n + ": " + resultantForce);
-                Vector3 acceleration = Vector3.ClampMagnitude(resultantForce, steerStrength) / 1; //normalised mass = 1
-                velocity = Vector3.ClampMagnitude(velocity + (acceleration * Time.deltaTime), maxSpeed);
-                Debug.Log("velocity" + n + ": " + velocity);
-
-                nextPosition += (velocity * Time.deltaTime);// + RandomStep();
-
-                //rbody.position += (velocity * Time.deltaTime);
-                rbody.MovePosition(nextPosition);
-               
-
-                // increment total path length
-                pathLength += (velocity * Time.deltaTime).magnitude;
-
-            }
-
-        }*/
-
     }
 
-=======
->>>>>>> 9a661de443f1963005796c3788be5320a0ba10bf
 }
 
 
